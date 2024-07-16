849516749416ca2cf791b25a05a2cc72caba01af
Ani1070git <135345359+Ani1070git@users.noreply.github.com>
2024-07-16 18:24:31 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // this is used to treat all the js code as newer version
+
+//alert(3+3); this does not works because we are using node not web brower
+
+console.log(3 + 3);//code readability is important
+
+let name = "Ani"
+let age = 20
+let isLoggedIn = true
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value has not be assinged
+//symbol =>unique
+
+
+//object
+
+console.log(typeof null);//object is the output
+console.log(typeof undefined);//undefined is the output
