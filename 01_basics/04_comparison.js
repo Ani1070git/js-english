17e42507effb74003e2aa559a808384035c4b672
Ani1070git <135345359+Ani1070git@users.noreply.github.com>
2024-08-03 06:22:18 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//equality ==  and comparison > <  works differently because comparison converts
+//the null into 0 whereas equality does not 
+
+// === this called strict check
+
+console.log("2" === 2);
