80753b7587da002b4619a5053845886a77e3463e
Ani1070git <135345359+Ani1070git@users.noreply.github.com>
2024-08-03 05:52:54 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 46
+
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 ; false => 0 ;
+
+let isLoggedIn = 1;
+//if we keeo the above value empty "" or 0 then it is false
+//but true for "ani"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn); 
+
+let someNumber = 90 ;
+
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+
+console.log(typeof StringNumber);
